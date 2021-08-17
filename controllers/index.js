4a6e535aeff220284f3b86d0e37e275b2035ccaf
EajayD <edelos1524@hotmail.com>
2021-08-16 19:21:03 -0700

    GET routes for exercise and stat page

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const exerciseRoutes = require('./exerciseRoutes');
+const statsRoutes = require('./statsRoutes');
 
 router.use('/api', apiRoutes);
+router.use('/exercise', exerciseRoutes);
+router.use('/stats', statsRoutes);
 
 module.exports = router;
